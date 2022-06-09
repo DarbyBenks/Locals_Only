433b888a2321def11b76da7f6af7b36f17f2719e
Darby Behnken <dtbehnken67@gmail.com>
2022-06-09 14:16:52 -0600

    login hover added

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -3,13 +3,14 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 function Profile() {
     const { user, isAuthenticated } = useAuth0()
-  
+    
   
     return (
+      
       isAuthenticated && (
         <div>
           <img src={user.picture} alt="Profile" /> 
-          <h1>Welcome, {user.name}!</h1>
+          <h1>Welcome, {user.name.toUpperCase()}!</h1>
 
             
         </div>
